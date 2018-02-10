f440fab732e7c4314fa8cab4dce836a9c7ba9cd7
benetta <ksenyatokareva@gmail.com>
2018-02-10 09:58:29 +0300

    добавила открытие модального окна по нажатию на корзину

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,23 @@
 
-var cart  = document.querySelector("#modal-open");
+var cart  = document.querySelectorAll(".modal-open");
 var popup = document.querySelector("#modal-order");
 var body  = document.querySelector("body");
 
-cart.addEventListener("click", function(evt) {
+cart.forEach(function(item) {
+  item.addEventListener("click", function(evt) {
   evt.preventDefault();
 
   body.classList.add("overlay");
   popup.classList.remove("modal--hidden");
+  });
 });
 
+
 // body.addEventListener("click", function(evt) {
-//   if(evt.target !== popup && !popup.contains(event.target)) {
-//     body.classList.remove("overlay");
-//     popup.classList.add("modal--hidden");
+//   if(popup.classList.contains("modal--hidden") === false) {
+//     if(evt.target !== popup && !popup.contains(event.target)) {
+//       body.classList.remove("overlay");
+//       popup.classList.add("modal--hidden");
+//     }
 //   }
 // });
