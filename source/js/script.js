ae1600fc2d0e174294a819122085984928ff515c
benetta <ksenyatokareva@gmail.com>
2018-02-13 17:28:25 +0300

    реализовала закрытие модального окна по клику

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,49 @@
+// модальное окно
 
 var cart  = document.querySelectorAll(".modal-open");
 var popup = document.querySelector("#modal-order");
-var body  = document.querySelector("body");
+var footer  = document.querySelector("footer");
 
 cart.forEach(function(item) {
   item.addEventListener("click", function(evt) {
   evt.preventDefault();
 
-  body.classList.add("overlay");
   popup.classList.remove("modal--hidden");
+  footer.classList.add("overlay");
   });
 });
 
+// браузер проверяет это условие, когда читает скрипт - 1 раз, после загрузки всего тела. поскольку ответом на if будет false, этот кусок кода никогда не запускается.
 
-// body.addEventListener("click", function(evt) {
-//   if(popup.classList.contains("modal--hidden") === false) {
-//     if(evt.target !== popup && !popup.contains(event.target)) {
-//       body.classList.remove("overlay");
-//       popup.classList.add("modal--hidden");
-//     }
-//   }
-// });
+// нужно сделать так, чтобы это условие запускалось каждый раз при открытии окна. Но, если вешать обработчик события на документ, и потом проверять условие, то он перебивает обработчик корзины.
+
+footer.addEventListener("click", function(evt) {
+  var isInside = (evt.target === popup);
+
+  if (!isInside && !popup.classList.contains("modal--hidden")) {
+    popup.classList.add("modal--hidden");
+    footer.classList.remove("overlay");
+  }
+});
+
+// if (!popup.classList.contains("modal--hidden")) {
+// }
+
+// меню на JS
+
+var menu = document.querySelectorAll(".main-nav__list");
+var toggle = document.querySelector("#main-nav__button");
+
+menu.forEach(function(item) {
+  item.classList.add("main-nav__list--hidden");
+})
+toggle.classList.remove("main-nav__toggle--no-js");
+
+toggle.addEventListener("click", function(evt) {
+  evt.preventDefault();
+
+  toggle.classList.toggle("main-nav__toggle--open");
+  menu.forEach(function(item) {
+    item.classList.toggle("main-nav__list--hidden");
+  })
+})
